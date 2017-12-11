f9c1a4011ae5dba18f4a351e2b7a19cc70b1a12f
Alex Salisbury <asalisbury@users.noreply.github.com>
2017-12-11 15:05:56 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import example1 from './example1';
+import example2 from './example2';
+import example3 from './example3';
+import example4 from './example4';
+import example5 from './example5';
+
+example1();
+example2();
+example3();
+example4();
+example5();
