f257f8bbcd79d57af14426b55c351f6a8142f136
Alex Salisbury <asalisbury@users.noreply.github.com>
2017-12-13 15:31:01 -0500

    Stub exercise 4

--- src/exercise4.ts
+++ src/exercise4.ts
@@ -10,7 +10,32 @@
 export default () => {
   // ======== Exercise 4.1 ========
   // Goals:
-  // • 
+  // • Create an interface and replace the param type with it
+  // • Make variantId optional
 
-  console.log('[Exercise 4.1]', 'sup');
+  function addToCart(item: { id: number, title: string, variantId: number }) {
+    console.log('[Exercise 4.1]', `Adding "${item.title}" to cart`);
+  }
+
+  addToCart({ id: 1, title: 'Concrete shoes' })
+
+
+  // ======== Exercise 4.2 ========
+  // Goals:
+  // • Create and implement an interface to enforce the structure of the Person class
+
+  class Person {
+    constructor(public name: string, public age: number) { }
+  }
+
+  const jane = new Person('Jane', 31);
+
+  console.log('[Exercise 4.2]', `${jane.name} is ${jane.age} years old.`);
+
+  // ======== Exercise 4.3 ========
+  // Goals:
+  // • Create and implement an interface to enforce the structure of the Person class
+
+
+  console.log('[Exercise 4.3]');
 }
